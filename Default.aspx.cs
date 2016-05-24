28d16bc4f453259f26d4d7d49b5a192a753b00d2
Jalen <JalenCBenjamin@gmail.com>
2016-05-24 16:42:45 -0400

    Add files via upload
    Minor edits have begun on Default.aspx, Default.aspx.cs, etc.

---
+++ Default.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+public sealed class MessageBox
+{ }
+
+public partial class _Default : Page
+{
+    MySql.Data.MySqlClient.MySqlConnection conn;
+    string myConnectionString = "server=127.0.0.1; uid=root;" + "pwd=Animegirl;database=anialt;";
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            conn = new MySql.Data.MySqlClient.MySqlConnection();
+            conn.ConnectionString = myConnectionString;
+            conn.Open();
+        }
+        catch (MySql.Data.MySqlClient.MySqlException ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        string search = txtProduct.Text;
+    }
+}
